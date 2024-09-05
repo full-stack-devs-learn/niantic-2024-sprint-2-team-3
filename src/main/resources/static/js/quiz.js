ae6283d813e84c1e6a6868c2533e51212d1dce56
Jane Huynh <janelehuynh1999@gmail.com>
2024-09-05 16:01:16 -0700

    Working on API and Javascript

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -1,4 +1,19 @@
+let questionPage = 1;
+let questionTotal = 1;
+
 document.addEventListener("DOMContentLoaded", () => {
     // on load
+    const quizId = document.getElementById("quiz-id").textContent;
+    const button = document.getElementById("quiz-button");
 
+    button.addEventListener("click", () => {
+
+    })
+
+    // change questionTotal based on amount of questions in quiz
+    fetch(`/api/quiz/${quizId}/questionCount`)
+        .then(response => response.text())
+        .then(data => {
+            questionTotal = +data;
+        });
 });
