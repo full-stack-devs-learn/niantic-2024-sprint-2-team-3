a61e541e87655a0a748ba1691f3669b3ec83872e
Jane Huynh <janelehuynh1999@gmail.com>
2024-09-05 13:31:30 -0700

    Able to Navigate to Quiz Page Correctly - did not put info or button yet

---
+++ src/main/java/com/nianti/controllers/QuizPageController.java
@@ -0,0 +1,23 @@
+package com.nianti.controllers;
+
+import com.nianti.models.Quiz;
+import com.nianti.services.QuizDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class QuizPageController {
+    QuizDao quizDao = new QuizDao();
+
+    @GetMapping("/quiz/{quizId}")
+    public String getQuizTitle(Model model, @PathVariable int quizId)
+    {
+        Quiz quiz = quizDao.getQuizById(quizId);
+
+        model.addAttribute("quiz", quiz);
+        return "quiz/quiz-page";
+    }
+}
