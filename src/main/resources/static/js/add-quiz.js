1e05c4071e036aea9be28a332338272c85c9d1f8
Jane Huynh <janelehuynh1999@gmail.com>
2024-09-06 20:23:07 -0700

    Quick fix - load js for add quiz validation

--- src/main/resources/static/js/add-quiz.js
+++ src/main/resources/static/js/add-quiz.js
@@ -4,10 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("add-quiz")
     const titleInput = document.getElementById("title")
 
-    /*titleInput.addEventListener("input",()=> {
-        form.classList.remove("was-validated")
-    })*/
-    console.log(form)
     form.addEventListener("submit", (event) => {
         if(!form.checkValidity())
         {
